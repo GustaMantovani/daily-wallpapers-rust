50d52cb14ede7175b19b4a4c01e5ee0c58892a0f
Gustavo Mantovani <mantovanig000@gmail.com>
2024-07-10 13:05:55 -0300

    🔨 updates the csc path

--- scripts/windows/build_wallpaper_changer.bat
+++ scripts/windows/build_wallpaper_changer.bat
@@ -3,7 +3,7 @@ if not exist external_builds\windows (
     mkdir external_builds\windows
 )
 
-csc /out:external_builds\windows\WallpaperChanger.exe src\windows\WallpaperChanger.cs
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:external_builds\windows\WallpaperChanger.exe src\windows\WallpaperChanger.cs
 
 if %errorlevel% neq 0 (
     echo Compilation failed!
